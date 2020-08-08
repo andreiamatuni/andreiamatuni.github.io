b2223097c3ded06a71d2fe1131b2d235e1517384
Andrei Amatuni <andrei.amatuni@gmail.com>
2020-08-08 13:09:49 -0500

    initial

---
+++ precache-manifest.a71620d0245f108a228d69c9a4f11b67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63f7d4ddedf9c26c4cf510f868ca9c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "512353912786b8b06b7c",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "73f8af335015272c7f82",
+    "url": "/static/css/main.2ed5935f.chunk.css"
+  },
+  {
+    "revision": "512353912786b8b06b7c",
+    "url": "/static/js/2.e370b404.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/static/js/2.e370b404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f8af335015272c7f82",
+    "url": "/static/js/main.09bad246.chunk.js"
+  },
+  {
+    "revision": "51d6ea71a5b92fdcb5a2",
+    "url": "/static/js/runtime-main.1fbbc959.js"
+  },
+  {
+    "revision": "045e84985e41863ae29ea7037e3abfd2",
+    "url": "/static/media/avatar.045e8498.jpg"
+  }
+]);
