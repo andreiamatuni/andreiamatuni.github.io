fad14f47198390312055c080fcfd0467f700c847
Andrei Amatuni <andrei.amatuni@gmail.com>
2023-01-02 02:16:44 -0600

    update

---
+++ precache-manifest.2acea94c1db5b947ddc1af81f14ccad5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68f0f97c7e9d6ec414aa7464c0b7257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de33000faef540d66b2",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "e78d93b1f5d971c84060",
+    "url": "/static/css/main.085d0410.chunk.css"
+  },
+  {
+    "revision": "4de33000faef540d66b2",
+    "url": "/static/js/2.7e131f7d.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/static/js/2.7e131f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78d93b1f5d971c84060",
+    "url": "/static/js/main.a698035b.chunk.js"
+  },
+  {
+    "revision": "51d6ea71a5b92fdcb5a2",
+    "url": "/static/js/runtime-main.1fbbc959.js"
+  },
+  {
+    "revision": "dc67cdb748ae771729d6b80d8d077074",
+    "url": "/static/media/avatar.dc67cdb7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeb930006f4885ba2fd588522ff30529.js"
+  "/precache-manifest.2acea94c1db5b947ddc1af81f14ccad5.js"
 );
 
 self.addEventListener('message', (event) => {
